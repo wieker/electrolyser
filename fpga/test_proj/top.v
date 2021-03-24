e782d36f35cfa51f03156e0d8cd488867057d295
kabramovich <wieker@gmail.com>
2021-03-24 22:32:48 +0200

    java => usb => spi. working FPGA firmware

--- fpga/test_proj/top.v
+++ fpga/test_proj/top.v
@@ -1,17 +1,9 @@
 module top(
-    output LED1, led2, led3,
-    input CLK
+    output LED1, led2, led3
 );
 
-    reg [16:0] counter;
-
-    always @(posedge CLK)
-        begin
-            counter <= counter + 1;
-        end
-
-    assign LED1 = counter[16];
-    assign led2 = counter[15];
-    assign led3 = counter[14];
+    assign LED1 = 1;
+    assign led2 = 0;
+    assign led3 = 1;
 
 endmodule
