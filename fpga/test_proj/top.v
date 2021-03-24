a5595c37ac8e60583af5743bcbf0a994b700e0f5
kabramovich <wieker@gmail.com>
2021-03-24 22:45:10 +0200

    java => usb => spi. XTAL test pass

--- fpga/test_proj/top.v
+++ fpga/test_proj/top.v
@@ -1,9 +1,17 @@
 module top(
-    output LED1, led2, led3
+    output LED1, led2, led3,
+    input CLK
 );
 
+    reg [16:0] counter;
+
+    always @(posedge CLK)
+        begin
+            counter <= counter + 1;
+        end
+
     assign LED1 = 1;
-    assign led2 = 0;
+    assign led2 = counter[11];
     assign led3 = 1;
 
 endmodule
