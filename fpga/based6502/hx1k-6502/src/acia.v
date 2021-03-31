d3c5455f05569a103168ef436662aafa33828448
kabramovich <wieker@gmail.com>
2021-04-01 00:48:42 +0300

    java => usb => uart => fpga. test transmission without the errors

--- fpga/based6502/hx1k-6502/src/acia.v
+++ fpga/based6502/hx1k-6502/src/acia.v
@@ -14,7 +14,7 @@ module acia(
 	output irq				// high-true interrupt request
 );
 	// hard-coded bit-rate
-	localparam sym_rate = 2400;
+	localparam sym_rate = 1200;
     localparam clk_freq = 32768;
     localparam sym_cnt = clk_freq / sym_rate;
 	localparam SCW = $clog2(sym_cnt);
