99f318a3228df83023d6f386c87d371d1666216f
kabramovich <wieker@gmail.com>
2021-04-11 19:35:07 +0300

    the third board descriptor

--- fpga/test_proj/top.v
+++ fpga/test_proj/top.v
@@ -1,17 +1,24 @@
 module top(
     output LED1, led2, led3,
-    input CLK
+    input CLK1, CLK2
 );
 
-    reg [21:0] counter;
+    reg [21:0] counter1;
 
-    always @(posedge CLK)
+    always @(posedge CLK1)
         begin
-            counter <= counter + 1;
+            counter1 <= counter1 + 1;
         end
 
-    assign LED1 = counter[20];
-    assign led2 = counter[21];
-    assign led3 = counter[19];
+    reg [21:0] counter2;
+
+    always @(posedge CLK2)
+        begin
+            counter2 <= counter2 + 1;
+        end
+
+    assign LED1 = counter2[21];
+    assign led2 = counter1[21];
+    assign led3 = counter1[19];
 
 endmodule
