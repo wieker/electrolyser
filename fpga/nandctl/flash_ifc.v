470f789b41dfb96aacb574f9eaed66e3e4a89b38
kabramovich <wieker@gmail.com>
2021-06-02 01:38:16 +0300

    starting NAND firmware: read status??? nw

--- fpga/nandctl/flash_ifc.v
+++ fpga/nandctl/flash_ifc.v
@@ -13,16 +13,16 @@ module flash_ifc(
 	output tx_start,
 );
 
-    reg [24:0] counter;
+    reg [25:0] counter;
     reg [4:0] waveform;
-    wire [3:0] state;
+    wire [4:0] state;
     assign {ce, we, re, cle, ale} = waveform;
-    assign state = counter[24:21];
+    assign state = counter[25:21];
 
     reg [7:0] dtr;
     assign din = dtr;
     reg [1:0] en;
-    assign tx_start = (state == 10) && (en < 3);
+    assign tx_start = (en > 0) && (en < 3);
 
     always @(posedge clk)
         begin
@@ -78,6 +78,89 @@ module flash_ifc(
                     en <= en + 1;
                 end
             end else if (state == 11) begin
+                waveform <= {1, 1, 1, 0, 0};
+                nand_oe <= 0;
+                nand_dout <= 8'h70;
+            end else if (state == 12) begin
+                waveform <= {1, 0, 1, 0, 0};
+                nand_oe <= 0;
+                nand_dout <= 8'hff;
+            end else if (state == 13) begin
+                waveform <= {0, 0, 1, 1, 0};
+                nand_oe <= 1;
+                nand_dout <= 8'h90;
+            end else if (state == 14) begin
+                waveform <= {0, 1, 1, 1, 0};
+                nand_oe <= 1;
+                nand_dout <= 8'h90;
+            end else if (state == 15) begin
+                waveform <= {1, 1, 1, 0, 0};
+                nand_oe <= 0;
+                nand_dout <= 8'hff;
+            end else if (state == 16) begin
+                waveform <= {1, 0, 1, 0, 0};
+                nand_oe <= 0;
+                nand_dout <= 8'hff;
+            end else if (state == 17) begin
+                waveform <= {0, 0, 1, 0, 1};
+                nand_oe <= 1;
+                nand_dout <= 8'h20;
+            end else if (state == 18) begin
+                waveform <= {0, 1, 1, 0, 1};
+                nand_oe <= 1;
+                nand_dout <= 8'h20;
+            end else if (state == 19) begin
+                waveform <= {1, 1, 1, 0, 0};
+                nand_oe <= 0;
+                nand_dout <= 8'hff;
+            end else if (state == 20) begin
+                waveform <= {0, 1, 0, 0, 0};
+                nand_oe <= 0;
+                nand_dout <= 8'hff;
+                en <= 0;
+            end else if (state == 21) begin
+                waveform <= {0, 1, 1, 0, 0};
+                nand_oe <= 0;
+                nand_dout <= 8'h70;
+                dtr <= nand_din;
+                if (en < 3) begin
+                    en <= en + 1;
+                end
+            end else if (state == 22) begin
+                waveform <= {1, 1, 1, 0, 0};
+                nand_oe <= 0;
+                nand_dout <= 8'h70;
+            end else if (state == 23) begin
+                waveform <= {0, 1, 0, 0, 0};
+                nand_oe <= 0;
+                nand_dout <= 8'hff;
+                en <= 0;
+            end else if (state == 24) begin
+                waveform <= {0, 1, 1, 0, 0};
+                nand_oe <= 0;
+                nand_dout <= 8'h70;
+                dtr <= nand_din;
+                if (en < 3) begin
+                    en <= en + 1;
+                end
+            end else if (state == 25) begin
+                waveform <= {1, 1, 1, 0, 0};
+                nand_oe <= 0;
+                nand_dout <= 8'h70;
+            end else if (state == 26) begin
+                waveform <= {0, 1, 0, 0, 0};
+                nand_oe <= 0;
+                nand_dout <= 8'hff;
+                en <= 0;
+            end else if (state == 27) begin
+                waveform <= {0, 1, 1, 0, 0};
+                nand_oe <= 0;
+                nand_dout <= 8'h70;
+                dtr <= nand_din;
+                if (en < 3) begin
+                    en <= en + 1;
+                end
+            end else if (state == 28) begin
                 waveform <= {1, 1, 1, 0, 0};
                 nand_oe <= 0;
                 nand_dout <= 8'h70;
