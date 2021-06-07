978d82b7944f6a3e4e7d74cfb43128f496beb74d
kabramovich <wieker@gmail.com>
2021-06-07 23:07:46 +0300

    nRF52810 ADC=>tx code works for the v1 board

--- mcu/nrf52/common/nrf_local_adc.c
+++ mcu/nrf52/common/nrf_local_adc.c
@@ -94,7 +94,7 @@ void saadc_callback(nrf_drv_saadc_evt_t const * p_event)
 void saadc_init(void)
 {
   nrf_saadc_channel_config_t channel_config =
-          NRF_DRV_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN0);
+          NRF_DRV_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN4);
 
   nrf_drv_saadc_init(NULL, saadc_callback);
 
