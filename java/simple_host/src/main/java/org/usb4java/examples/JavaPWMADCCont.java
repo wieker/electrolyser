97d19a3f3b4b43e601164f2423a5fdd108199e89
kabramovich <wieker@gmail.com>
2021-06-24 13:16:32 +0300

    pwm custom coil

---
+++ java/simple_host/src/main/java/org/usb4java/examples/JavaPWMADCCont.java
@@ -0,0 +1,61 @@
+package org.usb4java.examples;
+
+import org.usb4java.Device;
+import org.usb4java.DeviceHandle;
+import org.usb4java.LibUsb;
+import org.usb4java.LibUsbException;
+
+import static org.usb4java.examples.Ctl1.readKey;
+import static org.usb4java.examples.SimplePing.findDevice;
+import static org.usb4java.examples.SimplePing.sendCommand;
+
+public class JavaPWMADCCont {
+    public static void main(String[] args) {
+        int result = LibUsb.init(null);
+        if (result != LibUsb.SUCCESS) {
+            throw new LibUsbException("Unable to initialize libusb", result);
+        }
+
+        Device device = findDevice();
+        if (device == null) {
+            System.err.println("Missile launcher not found.");
+            System.exit(1);
+        }
+
+        DeviceHandle handle = new DeviceHandle();
+        result = LibUsb.open(device, handle);
+        if (result != LibUsb.SUCCESS) {
+            throw new LibUsbException("Unable to open USB device", result);
+        } try {
+            int attached = LibUsb.kernelDriverActive(handle, 1);
+            if (attached < 0) {
+                throw new LibUsbException( "Unable to check kernel driver active", result);
+            }
+
+            result = LibUsb.detachKernelDriver(handle, 0);
+            if (result != LibUsb.SUCCESS && result != LibUsb.ERROR_NOT_SUPPORTED && result != LibUsb.ERROR_NOT_FOUND) {
+                throw new LibUsbException("Unable to detach kernel driver", result);
+            }
+
+            result = LibUsb.claimInterface(handle, 0);
+            if (result != LibUsb.SUCCESS) {
+                throw new LibUsbException("Unable to claim interface", result);
+            }
+
+
+            while (true) {
+                System.out.print("> ");
+                sendCommand(handle, 2, new byte[]{ 0x00, (byte) 0x02 }, true);
+                Thread.sleep(1000l);
+                sendCommand(handle, 2, new byte[]{ 0x00, (byte) 0x04 }, true);
+                Thread.sleep(1000l);
+                }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally
+        {
+            LibUsb.close(handle);
+            LibUsb.exit(null);
+        }
+    }
+}
