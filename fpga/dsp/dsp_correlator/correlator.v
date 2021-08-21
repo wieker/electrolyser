07b70b4a3342e06e82d52f02f9c9540a9451ced6
kabramovich <wieker@gmail.com>
2021-08-21 16:38:16 +0300

    started LVDS rx: trying to start correlator

--- fpga/dsp/dsp_correlator/correlator.v
+++ fpga/dsp/dsp_correlator/correlator.v
@@ -15,9 +15,14 @@ module correlator(
 		    match_counter <= 0;
 		    result <= 0;
 		    rdy <= 0;
-		end else if (match_counter[10] == 1) begin
+		end else if ((match_counter[26] == 1) && rdy == 0) begin
 		    result <= miss_counter[13:6];
 		    rdy <= 1;
+		end else if (rdy == 1) begin
+		    miss_counter <= 0;
+		    match_counter <= 0;
+		    result <= 0;
+		    rdy <= 0;
 		end else begin
 		    match_counter <= match_counter + (code == sig);
 		    miss_counter <= miss_counter + (code != sig);
