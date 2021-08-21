76e12fa8cbfd354a6fadac1ab6c373043f2221af
kabramovich <wieker@gmail.com>
2021-08-22 00:56:20 +0300

    started LVDS rx: trying to start correlator

--- fpga/dsp/dsp_correlator/dispatcher.v
+++ fpga/dsp/dsp_correlator/dispatcher.v
@@ -6,14 +6,39 @@ module dispatcher(
     wire codes[8];
     wire [7:0] rdy;
     wire [7:0] results[8];
-    wire ready;
+    wire ready = rdy_tmp[7];
     genvar j;
     for (j=0; j < 8; j++) begin
 	    sig_source sig_source(.clk(clk), .rst(rst | rdy_tmp[7]), .period0(48 * 1024), .period1(48*1024), .phase(j * 6 * 1024), .start_code(0), .code(codes[j]));
         correlator correlator(.clk(clk), .rst(rst | rdy_tmp[7]), .sig(sig_in), .code(codes[j]), .rdy(rdy[j]), .result(results[j]));
     end
 
-    hex_dump hex_dump(.clk(clk), .rst(rst), .rdy(rdy), .fpga_tx(fpga_tx));
+    reg [2:0] state;
+    wire [2:0] next_state;
+    for (j=0; j < 8; j++) begin
+	    if ((state == j) && ready && !tx_busy) begin
+	        assign next_state == j + 1;
+	    end
+    end
+
+    wire tx_busy;
+    localparam sym_rate = 1200;
+    localparam clk_freq = 48000000;
+    localparam sym_cnt = clk_freq / sym_rate;
+    localparam SCW = $clog2(sym_cnt);
+
+    acia_tx #(
+        .SCW(SCW),              // rate counter width
+        .sym_cnt(sym_cnt)       // rate count value
+    )
+    my_tx(
+        .clk(clk),				// system clock
+        .rst(rst),			// system reset
+        .tx_dat(sym_tmp[7]),           // transmit data byte
+        .tx_start(rdy_tmp[7]),    // trigger transmission
+        .tx_serial(fpga_tx),         // tx serial output
+        .tx_busy(tx_busy)       // tx is active (not ready)
+    );
 
     genvar j;
     wire rdy_tmp[8];
