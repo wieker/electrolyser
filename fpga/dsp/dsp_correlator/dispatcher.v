1f5330e906f0cdf8b723e767da4ff71f33db6a61
kabramovich <wieker@gmail.com>
2021-08-22 21:01:10 +0300

    started LVDS rx: trying to start correlator: DSP UI

--- fpga/dsp/dsp_correlator/dispatcher.v
+++ fpga/dsp/dsp_correlator/dispatcher.v
@@ -3,36 +3,39 @@ module dispatcher(
     output fpga_tx,
 );
 
-    wire codes[8];
-    wire [7:0] rdy;
-    wire [7:0] results[8];
-    wire ready = rdy_tmp[7];
+    wire codes[10];
+    wire rdy[10];
+    wire [7:0] results[10];
+    wire ready = rdy_tmp[9];
     genvar j;
+
     for (j=0; j < 8; j++) begin
-	    sig_source sig_source(.clk(clk), .rst(rst | restart), .period0(48 * 1024), .period1(48*1024), .phase(j * 6 * 1024), .start_code(0), .code(codes[j]));
-        correlator correlator(.clk(clk), .rst(rst | restart), .sig(sig_in), .code(codes[j]), .rdy(rdy[j]), .result(results[j]));
+	    sig_source sig_source(.clk(clk), .rst(rst), .period0(48 * 1024), .period1(48*1024), .phase(j * 6 * 1024), .start_code(0), .code(codes[j]));
+    end
+    sig_source sig_source0(.clk(clk), .rst(rst), .period0(48 * 1024), .period1(1), .phase(0), .start_code(0), .code(codes[8]));
+    sig_source sig_source1(.clk(clk), .rst(rst), .period0(1), .period1(48 * 1024), .phase(0), .start_code(1), .code(codes[9]));
+
+    for (j=0; j < 10; j++) begin
+        correlator correlator(.clk(clk), .rst(rst), .sig(sig_in), .code(codes[j]), .rdy(rdy[j]), .result(results[j]));
     end
 
-    reg [2:0] state;
+    reg [3:0] state;
     reg tx_start;
     reg [7:0] symb;
-    reg restart;
+    reg started;
 
     always@(posedge clk)
     begin
-        if (rst) begin
+        started <= ready | started;
+        if (rst || !started) begin
             state <= 0;
-        end else if ((tx_start == 1) && (state == 0)) begin
-            restart <= 1;
-            tx_start <= 0;
-        end else if (tx_start == 1) begin
-            tx_start <= 0;
-        end else if ((restart == 1) && (state == 0)) begin
-            restart <= 0;
-        end else if (((ready == 1) || (state != 0)) && (tx_busy == 0) && (restart == 0)) begin
+        end else if (state == 10) begin
+        end else if (tx_busy == 0) begin
             tx_start <= 1;
             symb <= results[state];
             state <= state + 1;
+        end else begin
+            tx_start <= 0;
         end
     end
 
@@ -56,8 +59,8 @@ module dispatcher(
     );
 
     genvar j;
-    wire rdy_tmp[8];
-    for (j=0; j < 8; j++) begin
+    wire rdy_tmp[10];
+    for (j=0; j < 10; j++) begin
         if (j == 0)
             assign rdy_tmp[j] = rdy[0];
         else
