f1c0a4328264b63de92949ea92f71a12034bafd2
kabramovich <wieker@gmail.com>
2021-08-24 00:32:39 +0300

    started LVDS rx: trying to start correlator: preparing DSP UI: 6502 bus / uart reg dump/set

--- fpga/dsp/dsp_correlator/correlator.v
+++ fpga/dsp/dsp_correlator/correlator.v
@@ -1,14 +1,12 @@
 module correlator(
     input clk, rst, sig, code,
-    output reg rdy,
+    input capture,
     output reg [31:0] result_miss,
     output reg [31:0] result_match,
 );
 
     reg [31:0] miss_counter;
     reg [31:0] match_counter;
-    reg [31:0] counter;
-
 
 	always @(posedge clk)
 	begin
@@ -16,20 +14,14 @@ module correlator(
 		begin
 		    miss_counter <= 0;
 		    match_counter <= 0;
-		    counter <= 0;
 		    result_miss <= 0;
 		    result_match <= 0;
-		    rdy <= 0;
 		end else begin
-		    counter <= counter + 1;
 		    match_counter <= match_counter + (code == sig);
 		    miss_counter <= miss_counter + (code != sig);
-		    if ((counter[26] == 1) && rdy == 0) begin
+		    if (capture == 1) begin
 		        result_miss <= miss_counter;
 		        result_match <= match_counter;
-		        rdy <= 1;
-		    end else if (rdy == 1) begin
-		        rdy <= 0;
 		    end
 		end
 	end
