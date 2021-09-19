a15d417de820b35a71dc1e2aa238e4ab4d6afa63
kabramovich <wieker@gmail.com>
2021-09-19 23:38:56 +0300

    temporary broken commit: started experiments with phase tracking / sig selection

--- fpga/dsp/dsp_correlator/dispatcher.v
+++ fpga/dsp/dsp_correlator/dispatcher.v
@@ -6,26 +6,24 @@ module dispatcher(
     output rdy,
 );
 
-    wire codes[8];
-    wire [7:0] results[8];
+    wire codes[16];
+    wire [7:0] results[16];
+
     assign codes[0] = 0;
     assign codes[1] = 1;
-
-    sig_source sig_source(.clk(clk), .rst(rst), .period(8), .phase(0), .start_code(0), .code(codes[2]));
-    sig_source sig_sourceB(.clk(clk), .rst(rst), .period(6), .phase(0), .start_code(0), .code(codes[3]));
-
     genvar j;
-    for (j=4; j < 8; j++) begin
-        sig_source sig_source(.clk(clk), .rst(rst), .period(6), .phase(2 + j % 2), .start_code(j < 6), .code(codes[j]));
+    for (j=0; j < 6; j++) begin
+        sig_source sig_source(.clk(clk), .rst(rst), .half_period(6), .phase((j % 3) * 2), .start_code(j < 3), .code(codes[2 + j]));
+    end
+    for (j=0; j < 8; j++) begin
+        sig_source sig_source(.clk(clk), .rst(rst), .half_period(8), .phase((j % 4) * 2), .start_code(j < 4), .code(codes[8 + j]));
     end
 
-    for (j=0; j < 8; j++) begin
+    for (j=0; j < 16; j++) begin
         correlator correlator(.clk(clk), .rst(rst), .sig(sig), .code(codes[j]), .capture(capture), .select(addr_reg[0]), .result(results[j]));
     end
 
-    wire [7:0] data_out1 = addr_reg[2] ? addr_reg[1] ? results[3] : results[2] : addr_reg[1] ? results[1] : results[0];
-    wire [7:0] data_out2 = addr_reg[2] ? addr_reg[1] ? results[7] : results[6] : addr_reg[1] ? results[5] : results[4];
-    assign data_out = addr_reg[3] ? data_out2 : data_out1;
+    assign data_out = results[addr_reg[4:1]];
 
 
     wire capture;
@@ -41,15 +39,12 @@ module dispatcher(
         else if (!capture)
             ctr <= next_ctr;
     end
+
+    reg [7:0] addr_reg;
     always@(posedge clk)
     begin
         if (addr_latch)
             addr_reg <= data_in;
-        else if (data_rdy)
-            data_rdy <= 0;
-        else if (data_out_latch) begin
-            data_out <= correlator_out;
-            data_rdy <= 1;
         end
     end
 
