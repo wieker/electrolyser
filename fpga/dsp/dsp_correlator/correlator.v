5f949a79f83d230bcc07b6d35c43a46fc3ab0be6
kabramovich <wieker@gmail.com>
2021-09-23 01:30:47 +0300

    Revert "temporary broken commit: started experiments with phase tracking / sig selection"
    This reverts commit bc8328af

--- fpga/dsp/dsp_correlator/correlator.v
+++ fpga/dsp/dsp_correlator/correlator.v
@@ -1,20 +1,33 @@
 module correlator(
     input clk, rst, sig, code,
-    input [15:0] threshold,
-    output matched,
+    input capture,
+    input select,
+    output [7:0] result,
 );
 
+    reg [15:0] miss_counter;
     reg [15:0] match_counter;
+    reg [15:0] result_miss;
+    reg [15:0] result_match;
 
-    assign matched = match_counter == threshold;
+    assign result = select ? result_match[15:8] : result_match[7:0];
 
 	always @(posedge clk)
 	begin
 		if(rst)
 		begin
+		    miss_counter <= 0;
 		    match_counter <= 0;
+		    result_miss <= 0;
+		    result_match <= 0;
+		end else if (capture == 1) begin
+            result_miss <= miss_counter;
+            result_match <= match_counter;
+		    match_counter <= code == sig;
+		    miss_counter <= code != sig;
 		end else begin
 		    match_counter <= match_counter + (code == sig);
+		    miss_counter <= miss_counter + (code != sig);
 		end
 	end
 
