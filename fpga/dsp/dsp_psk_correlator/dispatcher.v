83023f2a798a7a1b756ac3cc5d19ae25f4cc8329
kabramovich <wieker@gmail.com>
2021-10-01 23:29:18 +0300

    preparing FSK and phase experiments: phase lock

--- fpga/dsp/dsp_psk_correlator/dispatcher.v
+++ fpga/dsp/dsp_psk_correlator/dispatcher.v
@@ -1,19 +1,26 @@
 module dispatcher(
     input clk, rst_in, sig,
-    output [7:0] value,
+    output reg [7:0] value,
     output stb,
 );
     reg [0:15] base_sig;
+    wire code;
     always@(posedge clk)
     begin
         if (rst_in) begin
             base_sig <= 16'h00ff;
         end else begin
-            base_sig <= {base_sig[1:15], base_sig[0]};
+            base_sig <= {base_sig[1:15], code};
         end
     end
 
-    correlator correlator(.clk(clk), .rst(rst), .sig(sig), .code(base_sig[3]), .value(value));
+    sigsv2 sigsv2(.clk(clk), .rst(rst_in), .left(stb && !value[3]), .right(stb && !value[5]), .code(code));
+
+    wire [7:0] rdys;
+    genvar j;
+    for (j=0; j < 8; j++) begin
+        correlator correlator(.clk(clk), .rst(rst), .sig(sig), .code(base_sig[2 * j]), .match(rdys[j]));
+    end
 
     wire rst;
     dispatcher_ctl ctl(
@@ -22,6 +29,18 @@ module dispatcher(
         .rst_out(rst),
         .stb(stb),
     );
+
+    reg [7:0] temp;
+
+    always@(posedge clk)
+    begin
+        if (rst) begin
+            value <= temp;
+            temp <= 0;
+        end else begin
+            temp <= temp | rdys;
+        end
+    end
 
 
 endmodule
