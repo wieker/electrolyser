3ca736dab9bd513c00b41f2741ec4f149fe18b82
kabramovich <wieker@gmail.com>
2021-10-03 13:21:35 +0300

    preparing FSK and phase experiments: synthesized with iceCube2

--- fpga/dsp/lvds_in/top.v
+++ fpga/dsp/lvds_in/top.v
@@ -22,26 +22,12 @@ module top(
     );
     reg [1:0] digitizer;
 
-    always@(posedge clk)
-    begin
-      digitizer <= { digitizer[0], comp_in };
-    end
-
     wire sig = digitizer[1];
     assign LED1 = sig;
 
-    reg [15:0] shift;
+    reg [7:0] shift;
 
-    initial begin
-        shift = 16'h00ff;
-    end
-
-    always@(posedge clk)
-    begin
-        shift <= { shift[14:0], shift[15] };
-    end
-
-    wire code = shift[15];
+    wire code = shift[0];
 
     reg [13:0] match_counter;
     reg [13:0] match_stage;
@@ -51,30 +37,25 @@ module top(
     reg [15:0] counter;
     wire [15:0] next = counter + 1;
 
-    always @(posedge clk)
-    begin
-        match_stage <= match_counter;
-        match <= (match_stage[10] == 1) && (match_stage[8] == 1);
-        sig_buf <= sig;
-        code_buf <= code;
-        if (next[11] == 1) begin
-            match_counter <= (code_buf == sig_buf);
-            counter <= 0;
-        end else begin
-            match_counter <= match_counter + (code_buf == sig_buf);
-            counter <= next;
-        end
-    end
-
     reg temp;
     reg value;
 
     always@(posedge clk)
     begin
+      digitizer <= { digitizer[0], comp_in };
+        shift <= { shift[6:0], !shift[7] };
+        match_stage <= match_counter;
+        match <= (match_stage[10] == 1) && (match_stage[8] == 1);
+        sig_buf <= sig;
+        code_buf <= code;
         if (next[11] == 1) begin
             value <= temp;
             temp <= 0;
+            match_counter <= (code_buf == sig_buf);
+            counter <= 0;
         end else begin
+            match_counter <= match_counter + (code_buf == sig_buf);
+            counter <= next;
             temp <= temp | match;
         end
     end
