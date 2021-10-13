c560f35275513b2e737a586e320a0860a2c995dd
kabramovich <wieker@gmail.com>
2021-10-14 00:34:33 +0300

    preparing FSK and phase experiments: started NCO phase lock attempt

--- fpga/dsp/dsp_psk_correlator/dispatcher.v
+++ fpga/dsp/dsp_psk_correlator/dispatcher.v
@@ -3,18 +3,16 @@ module dispatcher(
     output reg [7:0] value,
     output stb,
 );
-    wire i_code, q_code, b_code;
+    wire i_code, q_code;
     reg [12:0] fcw;
     reg [12:0] pcw;
     nco i_nco(.clk(clk), .rst(rst_in), .control_word(fcw), .i_code(i_code), .phase_control_word(pcw));
     nco q_nco(.clk(clk), .rst(rst_in), .control_word(fcw), .i_code(q_code), .phase_control_word(pcw + 13'b0100000000000));
-    nco b_nco(.clk(clk), .rst(rst_in), .control_word(fcw), .i_code(b_code), .phase_control_word(pcw + 13'b1100000000000));
 
     wire [7:0] i_value;
-    wire i_match, q_match, b_match;
-    correlator i_correlator(.clk(clk), .rst(rst), .sig(sig), .code(i_code), .value(i_value), .match(i_match));
-    correlator q_correlator(.clk(clk), .rst(rst), .sig(sig), .code(q_code), .match(q_match));
-    correlator b_correlator(.clk(clk), .rst(rst), .sig(sig), .code(b_code), .match(b_match));
+    wire [7:0] q_value;
+    correlator i_correlator(.clk(clk), .rst(rst), .sig(sig), .code(i_code), .value(i_value));
+    correlator q_correlator(.clk(clk), .rst(rst), .sig(sig), .code(q_code), .value(q_value));
 
     wire rst;
     dispatcher_ctl ctl(
@@ -24,29 +22,29 @@ module dispatcher(
         .stb(stb)
     );
 
-    reg q_save, b_save, i_save;
+    wire q1 = (i_value[7] == 1) && ((i_value[6] == 1) || (i_value[5] == 1));
+    wire q3 = (i_value[7] == 0) && ((i_value[6] == 0) || (i_value[5] == 0));
+    wire q2 = (q_value[7] == 1) && ((q_value[6] == 1) || (q_value[5] == 1));
+    wire q4 = (q_value[7] == 0) && ((q_value[6] == 0) || (q_value[5] == 0));
     always@(posedge clk)
     begin
         if (rst_in) begin
             fcw <= 13'b0001000000000;
             pcw <= 13'b0000000000000;
-        end else if (rst) begin
-            value <= {3'b100, i_save, 2'b00, q_save, b_save};
-            q_save <= 0;
-            b_save <= 0;
-            i_save <= 0;
-            if (i_value[7] == 0) begin
+        end else if (stb) begin
+            value <= i_value;
+            if (q1 && q2) begin
+                pcw <= pcw + 13'b0000100000000;
+            end else if (q1 && q4) begin
+                pcw <= pcw + 13'b1111100000000;
+            end else if (q1) begin
+            end else if (q2) begin
+                pcw <= pcw + 13'b0100000000000;
+            end else if (q4) begin
+                pcw <= pcw + 13'b1100000000000;
+            end else if (q3) begin
                 pcw <= pcw + 13'b1000000000000;
-            end else if (i_save) begin
-            end else if (q_save) begin
-                pcw <= pcw + 13'b0010000000000;
-            end else if (b_save) begin
-                pcw <= pcw + 13'b1110000000000;
             end
-        end else begin
-            q_save <= q_save | q_match;
-            b_save <= b_save | b_match;
-            i_save <= i_save | i_match;
         end
     end
 
