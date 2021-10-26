453404410618bf62226381c6d92a9a8ab1ecf7db
kabramovich <wieker@gmail.com>
2021-10-26 22:32:11 +0300

    preparing FSK and phase experiments: LVDS-rx => SPI dump => USB => SPI sig

--- fpga/dsp/dsp_psk_correlator/dispatcher.v
+++ fpga/dsp/dsp_psk_correlator/dispatcher.v
@@ -3,17 +3,16 @@ module dispatcher(
     output reg [7:0] value,
     output reg rdy,
 );
-    wire i_code, q_code, ref;
+    wire i_code, q_code;
     reg [12:0] fcw;
     reg [12:0] pcw;
-    nco ref_nco(.clk(clk), .rst(rst_in), .control_word(12'h101), .i_code(ref), .phase_control_word(12'h000));
     nco i_nco(.clk(clk), .rst(rst_in), .control_word(fcw), .i_code(i_code), .phase_control_word(pcw));
     nco q_nco(.clk(clk), .rst(rst_in), .control_word(fcw), .i_code(q_code), .phase_control_word(pcw + 12'h200));
 
     wire [7:0] i_value;
     wire [7:0] q_value;
-    correlator i_correlator(.clk(clk), .rst(rst), .sig(ref), .code(i_code), .value(i_value));
-    correlator q_correlator(.clk(clk), .rst(rst), .sig(ref), .code(q_code), .value(q_value));
+    correlator i_correlator(.clk(clk), .rst(rst), .sig(sig), .code(i_code), .value(i_value));
+    correlator q_correlator(.clk(clk), .rst(rst), .sig(sig), .code(q_code), .value(q_value));
 
     wire rst;
     dispatcher_ctl ctl(
--- fpga/dsp/dsp_psk_correlator/osc.v
+++ fpga/dsp/dsp_psk_correlator/osc.v
@@ -1,9 +1,7 @@
 module osc(
-    output clk, rst
+    output clk, rst,
 );
 
-    wire bclk;
-    reg [19:0] bcount;
 
     reg [3:0] startup;
     assign rst = !startup[3];
@@ -11,20 +9,8 @@ module osc(
     SB_HFOSC inthosc (
       .CLKHFPU(1'b1),
       .CLKHFEN(1'b1),
-      .CLKHF(bclk)
+      .CLKHF(clk)
     );
-
-    wire clk = bcount[9];
-
-    wire [19:0] bnext = bcount + 1;
-    always@(posedge bclk)
-    begin
-        if (bnext == 1000) begin
-            bcount <= 0;
-        end else begin
-            bcount <= bnext;
-        end
-    end
 
     always@(posedge clk)
     begin
