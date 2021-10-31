a01051910253d1f8256c80123509606fa6797b59
kabramovich <wieker@gmail.com>
2021-10-31 11:22:31 +0200

    preparing FSK and phase experiments: PSK demod skeleton

--- fpga/dsp/dsp_psk_correlator/dispatcher.v
+++ fpga/dsp/dsp_psk_correlator/dispatcher.v
@@ -1,28 +1,36 @@
 module dispatcher(
     input clk, rst_in, sig,
-    output [7:0] value,
-    output rdy,
+    output reg [7:0] value,
+    output reg rdy,
 );
 
+    reg [4:0] offsetl;
+    reg [4:0] offsetr;
     reg [4:0] offset;
 
     wire am_lstb;
     wire am_rstb;
     wire [7:0] lvalue;
     wire [7:0] rvalue;
-    am_detector left(.clk(clk), .rst_in(rst), .sig(sig), .offset(offset), .rdy(am_stb), .value(lvalue));
-    am_detector right(.clk(clk), .rst_in(rst), .sig(sig), .offset(offset), .rdy(am_rstb), .value(rvalue));
+    am_detector left(.clk(clk), .rst_in(rst), .sig(sig), .offset(offsetl), .rdy(am_stb), .value(lvalue));
+    am_detector right(.clk(clk), .rst_in(rst), .sig(sig), .offset(offsetr), .rdy(am_rstb), .value(rvalue));
     psk_demod demod(.clk(clk), .rst_in(rst), .sig(sig), .offset(offset), .rdy(rdy), .value(value));
 
 
     always@(posedge clk)
     begin
         if (rst_in) begin
+            offsetl <= 8;
+            offsetr <= 24;
             offset <= 16;
         end else if (am_lstb | am_rstb) begin
             if (lvalue > rvalue) begin
+                offsetl <= offset + 1;
+                offsetr <= offsetr + 1;
                 offset <= offset + 1;
             end else if (lvalue < rvalue) begin
+                offsetl <= offsetl + 5'b11111;
+                offsetr <= offsetr + 5'b11111;
                 offset <= offset + 5'b11111;
             end
         end
