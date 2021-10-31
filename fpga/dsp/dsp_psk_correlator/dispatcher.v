4199eafaeac742f3b487fd5247768e5d0c7635f1
kabramovich <wieker@gmail.com>
2021-10-31 03:12:27 +0300

    preparing FSK and phase experiments: pulse counter

--- fpga/dsp/dsp_psk_correlator/dispatcher.v
+++ fpga/dsp/dsp_psk_correlator/dispatcher.v
@@ -29,14 +29,9 @@ module dispatcher(
     reg q4;
 
     reg i_q;
-
-    reg [7:0] counter_i;
-    reg [7:0] counter_q;
-
-    reg [19:0] summ;
-
-    reg [15:0] summ_counter;
+    reg [19:0] pulse_counter;
     reg [4:0] offset;
+    reg [19:0] res_counter;
 
     always@(posedge clk)
     begin
@@ -47,40 +42,25 @@ module dispatcher(
             q1 <= i_value[7];
             q3 <= q_value[7];
             st1 <= 1;
+            res_counter ++;
         end else if (st1) begin
-            if (q2 == q1) begin
-                counter_i <= counter_i + 1;;
-            end
-            if (q4 == q3) begin
-                counter_q <= counter_q + 1;
-            end
-            if (q2 != q1) begin
-                q2 <= q1;
-                counter_i <= 1;
-            end
-            if (q4 != q3) begin
-                q4 <= q3;
-                counter_q <= 1;
-            end
             if ((q4 != q3) && i_q) begin
-                summ <= summ + counter_q;
                 i_q <= 0;
-                summ_counter <= summ_counter + 1;
+                pulse_counter <= pulse_counter + 1;
             end else if ((q2 != q1) && !i_q) begin
-                summ <= summ + counter_i;
                 i_q <= 1;
-                summ_counter <= summ_counter + 1;
+                pulse_counter <= pulse_counter + 1;
             end
             st1 <= 0;
-        end else if (summ_counter == 256 * 16) begin
-            summ_counter <= 0;
-            summ <= 0;
-            value <= summ[19:12];
+        end else if (res_counter[19] == 1) begin
+            pulse_counter <= 0;
+            res_counter <= 0;
+            value <= pulse_counter[19:12];
             rdy <= 1;
-            offset <= offset + 1;
             st2 <= 1;
         end else if (st2) begin
             st2 <= 0;
+            offset <= offset + 1;
             value <= offset;
         end else begin
             rdy <= 0;
