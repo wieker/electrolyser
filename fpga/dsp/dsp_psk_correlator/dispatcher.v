8962431b33550932784e2a2967accbac17c2f2fe
kabramovich <wieker@gmail.com>
2021-10-31 11:04:43 +0200

    preparing FSK and phase experiments: PSK demod skeleton

--- fpga/dsp/dsp_psk_correlator/dispatcher.v
+++ fpga/dsp/dsp_psk_correlator/dispatcher.v
@@ -1,75 +1,30 @@
 module dispatcher(
     input clk, rst_in, sig,
-    output reg [7:0] value,
-    output reg rdy,
+    output [7:0] value,
+    output rdy,
 );
-    wire i_code, q_code;
-    nco i_nco(.clk(clk), .rst(rst_in), .control_word(16'h0ff0 + offset), .i_code(i_code), .phase_control_word(16'h0000));
-    nco q_nco(.clk(clk), .rst(rst_in), .control_word(16'h0ff0 + offset), .i_code(q_code), .phase_control_word(16'h4000));
 
-    wire [7:0] i_value;
-    wire [7:0] q_value;
-    correlator i_correlator(.clk(clk), .rst(rst), .sig(sig), .code(i_code), .value(i_value));
-    correlator q_correlator(.clk(clk), .rst(rst), .sig(sig), .code(q_code), .value(q_value));
-
-    wire rst;
-    dispatcher_ctl ctl(
-        .clk(clk),
-        .rst_in(rst_in),
-        .rst_out(rst),
-        .stb(stb)
-    );
-
-    wire stb;
-    reg st1;
-    reg st2;
-    reg q1;
-    reg q2;
-    reg q3;
-    reg q4;
-
-    reg i_q;
-    reg [19:0] pulse_counter;
     reg [4:0] offset;
-    reg [19:0] res_counter;
+
+    wire am_lstb;
+    wire am_rstb;
+    wire [7:0] lvalue;
+    wire [7:0] rvalue;
+    am_detector left(.clk(clk), .rst_in(rst), .sig(sig), .offset(offset), .rdy(am_stb), .value(lvalue));
+    am_detector right(.clk(clk), .rst_in(rst), .sig(sig), .offset(offset), .rdy(am_rstb), .value(rvalue));
+    psk_demod demod(.clk(clk), .rst_in(rst), .sig(sig), .offset(offset), .rdy(rdy), .value(value));
+
 
     always@(posedge clk)
     begin
         if (rst_in) begin
-            rdy <= 0;
-            st1 <= 0;
-        end else if (stb) begin
-            q1 <= i_value[7];
-            q3 <= q_value[7];
-            st1 <= 1;
-            res_counter ++;
-        end else if (st1) begin
-            if (q2 != q1) begin
-                q2 <= q1;
+            offset <= 16;
+        end else if (am_lstb | am_rstb) begin
+            if (lvalue > rvalue) begin
+                offset <= offset + 1;
+            end else if (lvalue < rvalue) begin
+                offset <= offset + 5'b11111;
             end
-            if (q4 != q3) begin
-                q4 <= q3;
-            end
-            if ((q4 != q3) && i_q) begin
-                i_q <= 0;
-                pulse_counter <= pulse_counter + 1;
-            end else if ((q2 != q1) && !i_q) begin
-                i_q <= 1;
-                pulse_counter <= pulse_counter + 1;
-            end
-            st1 <= 0;
-        end else if (res_counter[19] == 1) begin
-            pulse_counter <= 0;
-            res_counter <= 0;
-            value <= pulse_counter[19:12];
-            rdy <= 1;
-            st2 <= 1;
-        end else if (st2) begin
-            st2 <= 0;
-            offset <= offset + 1;
-            value <= offset;
-        end else begin
-            rdy <= 0;
         end
     end
 
