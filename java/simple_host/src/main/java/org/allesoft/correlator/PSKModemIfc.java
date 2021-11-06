7eaa5d29c7340ac9c5be4919f1e1e6e167b24c11
kabramovich <wieker@gmail.com>
2021-11-06 14:59:28 +0200

    preparing FSK and phase experiments: started second board: QFN ice40 works

--- java/simple_host/src/main/java/org/allesoft/correlator/PSKModemIfc.java
+++ java/simple_host/src/main/java/org/allesoft/correlator/PSKModemIfc.java
@@ -22,7 +22,7 @@ public class PSKModemIfc
 
     private static final short VENDOR_ID = 0x6666;
 
-    private static final short PRODUCT_ID = 0x6671;
+    private static final short PRODUCT_ID = 0x6675;
     //private static final short PRODUCT_ID = 0x6668;
 
     private static final int TIMEOUT = 0;
