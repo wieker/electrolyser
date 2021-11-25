330b75277307a7acd1e08699a7e927bcf44903c6
kabramovich <wieker@gmail.com>
2021-11-26 01:00:10 +0200

    preparing FSK and phase experiments: started I/Q demod for DLL

--- fpga/dsp/dsp_DLL_correlator/dispatcher.v
+++ fpga/dsp/dsp_DLL_correlator/dispatcher.v
@@ -3,9 +3,9 @@ module dispatcher(
     output reg [7:0] value,
     output reg rdy,
 );
-    iq_demod early();
-    iq_demod late();
-    iq_demod actual(); //PSK modem
+    iq_demod early(.clk(clk), .rst_in(rst), .sig(sig), .rdy(stb), .value(value));
+    iq_demod late(.clk(clk), .rst_in(rst), .sig(sig), .rdy(stb), .value(value));
+    iq_demod actual(.clk(clk), .rst_in(rst), .sig(sig), .rdy(stb), .value(value)); //PSK modem
 
 
 endmodule
