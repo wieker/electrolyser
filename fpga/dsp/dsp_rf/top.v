475df4ac145aa20f1bce5ba3c0995218e18d2cb5
kabramovich <wieker@gmail.com>
2021-11-30 22:07:34 +0200

    preparing FSK and phase experiments: DLL => PSK sense

--- fpga/dsp/dsp_rf/top.v
+++ fpga/dsp/dsp_rf/top.v
@@ -26,7 +26,7 @@ module top(
     nco i_nco(.clk(CLK1), .rst(0), .control_word(16'h4000), .i_code(i_code), .phase_control_word(16'h0000));
     nco q_nco(.clk(CLK1), .rst(0), .control_word(16'h4000), .i_code(q_code), .phase_control_word(16'h4000));
 
-    assign rf = select ? i_code : q_code;
+    assign rf = select ? i_code : ~ i_code;
 
     always @(posedge CLK2)
     begin
