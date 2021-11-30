49fd0a9d173f35ba254f374e88f0f3605558e940
kabramovich <wieker@gmail.com>
2021-11-30 23:54:31 +0200

    preparing FSK and phase experiments: DLL => PSK sense

--- fpga/dsp/dsp_DLL_correlator/iabs_demod.v
+++ fpga/dsp/dsp_DLL_correlator/iabs_demod.v
@@ -4,14 +4,11 @@ module iabs_demod(
     output reg rdy,
     input [15:0] phase,
 );
-    wire i_code, q_code;
+    wire i_code;
     nco i_nco(.clk(clk), .rst(rst_in), .control_word(16'h1000), .i_code(i_code), .phase_control_word(16'h0000 + phase));
-    nco q_nco(.clk(clk), .rst(rst_in), .control_word(16'h1000), .i_code(q_code), .phase_control_word(16'h4000 + phase));
 
     wire [7:0] i_value;
-    wire [7:0] q_value;
     correlator i_correlator(.clk(clk), .rst(rst), .sig(sig), .code(i_code), .value(i_value));
-    correlator q_correlator(.clk(clk), .rst(rst), .sig(sig), .code(q_code), .value(q_value));
 
     wire rst;
     dispatcher_ctl ctl(
@@ -23,37 +20,22 @@ module iabs_demod(
 
     wire stb;
     reg st1;
-    reg st2;
     reg [7:0] q1;
-    reg [7:0] q2;
-    reg [7:0] q3;
-    reg [7:0] q4;
 
     always@(posedge clk)
     begin
         if (rst_in) begin
             rdy <= 0;
             st1 <= 0;
-            st2 <= 0;
         end else if (stb) begin
             q1 <= i_value[7] ? i_value : ~ i_value;
-            q2 <= q_value[7] ? q_value : ~ q_value;
             st1 <= 1;
         end else if (st1) begin
-            q3 <= {0, q1[6:0]};
-            q4 <= {0, q2[6:0]};
-            st1 <= 0;
-            st2 <= 1;
-        end else if (st2) begin
-            value <= q3;  // only I
-            //value <= q3 + q4;
+            value <= {0, q1[6:0]};
             rdy <= 1;
             st1 <= 0;
-            st2 <= 0;
         end else begin
             rdy <= 0;
-            st1 <= 0;
-            st2 <= 0;
         end
     end
 
--- fpga/dsp/dsp_DLL_correlator/psk_demod.v
+++ fpga/dsp/dsp_DLL_correlator/psk_demod.v
@@ -4,14 +4,11 @@ module psk_demod(
     output reg rdy,
     input [15:0] phase,
 );
-    wire i_code, q_code;
+    wire i_code;
     nco i_nco(.clk(clk), .rst(rst_in), .control_word(16'h1000), .i_code(i_code), .phase_control_word(16'h0000 + phase));
-    nco q_nco(.clk(clk), .rst(rst_in), .control_word(16'h1000), .i_code(q_code), .phase_control_word(16'h4000 + phase));
 
     wire [7:0] i_value;
-    wire [7:0] q_value;
     correlator i_correlator(.clk(clk), .rst(rst), .sig(sig), .code(i_code), .value(i_value));
-    correlator q_correlator(.clk(clk), .rst(rst), .sig(sig), .code(q_code), .value(q_value));
 
     wire rst;
     dispatcher_ctl ctl(
@@ -24,11 +21,7 @@ module psk_demod(
     wire stb;
     reg st1;
     reg q1;
-    reg q2;
-    reg q3;
-    reg q4;
 
-    reg i_q;
     reg [7:0] res_counter;
     reg [7:0] pulse_counter;
 
@@ -39,27 +32,14 @@ module psk_demod(
             st1 <= 0;
         end else if (stb) begin
             q1 <= i_value[7];
-            q3 <= q_value[7];
             st1 <= 1;
             res_counter ++;
         end else if (st1) begin
-            if (q2 != q1) begin
-                q2 <= q1;
-            end
-            if (q4 != q3) begin
-                q4 <= q3;
-            end
-            if ((q4 != q3) && i_q) begin
-                i_q <= 0;
-                pulse_counter <= pulse_counter + 1;
-            end else if ((q2 != q1) && !i_q) begin
-                i_q <= 1;
-                pulse_counter <= pulse_counter + 1;
-            end
+            pulse_counter <= pulse_counter + q1;
             st1 <= 0;
         end else if (res_counter == 0) begin
             res_counter <= 1;
-            value <= q1;
+            value <= pulse_counter;
             pulse_counter <= 0;
             rdy <= 1;
         end else begin
