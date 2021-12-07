3cea26dc07e8c38c0d7baa925529fd342250eff4
kabramovich <wieker@gmail.com>
2021-12-07 20:15:15 +0200

    preparing FSK and phase experiments: started codes correlator

--- fpga/dsp/dsp_rf/top.v
+++ fpga/dsp/dsp_rf/top.v
@@ -30,7 +30,7 @@ module top(
         end
 
     nco i_nco(.clk(CLK1), .rst(0), .control_word(16'h4000), .i_code(i_code), .phase_control_word(16'h0000));
-    nco q_nco(.clk(CLK1), .rst(0), .control_word(16'h0800), .i_code(q_code), .phase_control_word(16'h0000));
+    nco q_nco(.clk(CLK1), .rst(0), .control_word(16'h0080), .i_code(q_code), .phase_control_word(16'h0000));
 
     assign rf = i_code ^ q_code;
 
