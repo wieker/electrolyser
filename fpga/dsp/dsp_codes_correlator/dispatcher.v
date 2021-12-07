9514a994c2d478c42b5d6c3332bddd41f4b57e13
kabramovich <wieker@gmail.com>
2021-12-07 21:55:33 +0200

    preparing FSK and phase experiments: started codes correlator

--- fpga/dsp/dsp_codes_correlator/dispatcher.v
+++ fpga/dsp/dsp_codes_correlator/dispatcher.v
@@ -4,8 +4,14 @@ module dispatcher(
     output [7:0] q_value_reg,
     output rdy,
 );
-    iq_demod early(.clk(clk), .rst_in(rst_in), .sig(sig ^ e_code), .rdy(rdy), .value(i_value_reg), .phase(0));
-    iq_demod late(.clk(clk), .rst_in(rst_in), .sig(sig ^ l_code), .rdy(rrdy), .value(q_value_reg), .phase(0));
+    wire [7:0] early_wire;
+    wire [7:0] late_wire;
+
+    assign i_value_reg = early_wire + late_wire;
+
+    iq_demod early(.clk(clk), .rst_in(rst_in), .sig(sig ^ e_code), .rdy(lrdy), .value(early_wire), .phase(0));
+    iq_demod late(.clk(clk), .rst_in(rst_in), .sig(sig ^ l_code), .rdy(rrdy), .value(late_wire), .phase(0));
+    iq_demod direct(.clk(clk), .rst_in(rst_in), .sig(sig), .rdy(rdy), .value(q_value_reg), .phase(0));
 
     wire e_code, l_code;
     nco e_nco(.clk(clk), .rst(rst_in), .control_word(16'h0400), .i_code(e_code), .phase_control_word(16'h0000));
