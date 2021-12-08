145b5f54c5ebec3198ef026ae293f83a5c9b0427
kabramovich <wieker@gmail.com>
2021-12-08 22:39:48 +0200

    preparing FSK and phase experiments: started codes correlator

--- fpga/dsp/dsp_codes_correlator/dispatcher.v
+++ fpga/dsp/dsp_codes_correlator/dispatcher.v
@@ -1,11 +1,16 @@
 module dispatcher(
     input clk, rst_in, sig,
-    output [7:0] i_value_reg,
-    output [7:0] q_value_reg,
-    output rdy,
+    output reg [7:0] i_value_reg,
+    output reg [7:0] q_value_reg,
+    output reg rdy,
 );
-    iq_demod early(.clk(clk), .rst_in(rst_in), .sig(sig ^ code[0]), .rdy(lrdy), .value(q_value_reg), .phase(0));
-    iq_demod late(.clk(clk), .rst_in(rst_in), .sig(sig ^ code[1]), .rdy(rdy), .value(i_value_reg), .phase(0));
+
+    wire [7:0] value[6];
+    wire rdys[6];
+    genvar i;
+    for (i = 0; i < 6; i ++) begin
+        iq_demod i0(.clk(clk), .rst_in(rst_in), .sig(sig ^ code[i]), .rdy(rdys[i]), .value(value[i]), .phase(0));
+    end
 
     wire e_code, l_code;
     nco e_nco(.clk(clk), .rst(rst_in), .control_word(16'h0800), .i_code(e_code), .phase_control_word(16'h0000));
@@ -20,5 +25,29 @@ module dispatcher(
                 code <= {code[4:0], code[5]};
             end
         end
+
+    reg [2:0] pipeline;
+    always@(posedge clk)
+    begin
+        if (rdys[0]) begin
+            pipeline[0] <= 1;
+            i_value_reg <= value[0];
+            q_value_reg <= value[1];
+            rdy <= 1;
+        end else if (pipeline[0]) begin
+            pipeline[0] <= 0;
+            pipeline[1] <= 1;
+            i_value_reg <= value[2];
+            q_value_reg <= value[3];
+        end else if (pipeline[1]) begin
+            pipeline[1] <= 0;
+            pipeline[2] <= 1;
+            i_value_reg <= value[4];
+            q_value_reg <= value[5];
+        end else if (pipeline[2]) begin
+            pipeline[2] <= 0;
+            rdy <= 0;
+        end
+    end
 
 endmodule
