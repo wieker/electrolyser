aa3713b3ab69b4d17a175f6fb3ce8365454ced0d
kabramovich <wieker@gmail.com>
2021-12-25 20:02:08 +0200

    power logger: started isolated-rx => SPIdumper => usb: rfdump => spiflash

--- fpga/dsp/dsp_power_dumper/hex_dump.v
+++ fpga/dsp/dsp_power_dumper/hex_dump.v
@@ -4,90 +4,67 @@ module hex_dump(
     output SPI_SCK, output SPI_SS, output SPI_MOSI, input SPI_MISO,
 );
 
-    assign rdy4 = SPI_MISO;
-    assign rdy3 = SPI_SCK;
+    assign rdy4 = done;
+    assign rdy3 = sig;
+    wire [7:0] i_value;
+    wire [7:0] q_value;
 
-    reg [15:0] ram_addr;
+    dispatcher dispatcher(.clk(clk), .rst_in(rst), .sig(sig), .rdy(stb),
+        .i_value_reg(i_value), .q_value_reg(q_value));
 
-    reg tx_start;
-    reg [7:0] touart;
-    reg bugfix001;
-    reg [1:0] state;
+    reg [10:0] ram_addr;
+    wire [15:0] ram_data_in = {q_value[7:0], i_value[7:0]};
+    wire [15:0] ram_data_out;
+    wire ram_wren = (!ram_addr[10] && stb) & ~ done;
+
+    SB_SPRAM256KA spram
+    (
+        .ADDRESS({6'h00, ram_addr[9:0]}),
+        .DATAIN(ram_data_in),
+        .MASKWREN({ram_wren, ram_wren, ram_wren, ram_wren}),
+        .WREN(ram_wren),
+        .CHIPSELECT(1),
+        .CLOCK(clk),
+        .STANDBY(1'b0),
+        .SLEEP(1'b0),
+        .POWEROFF(1'b1),
+        .DATAOUT(ram_data_out)
+    );
+
+    reg done;
 
     always@(posedge clk)
     begin
-        if (rst) begin
-
-        end else if ((ram_addr[10] == 0) && (spi_rd_data_available != rd_data_available_old) && !tx_busy && !bugfix001) begin
+        if (ram_addr[10]) begin
+            done <= 1;
+        end else if (rst) begin
+            done <= 0;
+        end
+        if (ram_wren) begin
+            ram_addr <= ram_addr + 1;
+        end else if (ram_addr[10] && (rd_data_available_old != spi_rd_data_available)) begin
             rd_data_available_old <= spi_rd_data_available;
             spi_rd_ack <= ~ spi_rd_ack;
-            bugfix001 <= 1;
-            tx_start <= 1;
-            touart <= ram_addr[7:0];
+            wr_data <= ram_data_out;
             ram_addr <= ram_addr + 1;
-            state <= state + 1;
-            spi_data <= ram_addr;
-        end else begin
-            if (!tx_busy && bugfix001) begin
-                bugfix001 <= 0;
-            end
-            tx_start <= 0;
         end
     end
 
-    wire tx_busy;
-    localparam sym_rate = 1200;
-    localparam clk_freq = 48000000;
-    localparam sym_cnt = clk_freq / sym_rate;
-    localparam SCW = $clog2(sym_cnt);
-
-    acia_tx #(
-        .SCW(SCW),              // rate counter width
-        .sym_cnt(sym_cnt)       // rate count value
-    )
-    my_tx(
-        .clk(clk),				// system clock
-        .rst(rst),			// system reset
-        .tx_dat(touart),           // transmit data byte
-        .tx_start(tx_start),    // trigger transmission
-        .tx_serial(fpga_tx),         // tx serial output
-        .tx_busy(tx_busy)       // tx is active (not ready)
-    );
-
-    reg spi_rd;
-    reg spi_ack;
-    reg [15:0] spi_data;
-
-    reg [20:0] clk_counter;
-    reg xclock;
-    always @(posedge clk) begin
-        clk_counter <= clk_counter + 1;
-        if (xclock == 0 && clk_counter[4] == 0) begin
-            xclock <= 1;
-            spi_ack <= spi_ack | spi_rd_ack;
-            spi_rd <= spi_rd_data_available;
-            wr_data <= spi_data;
-        end else if (xclock == 1 && lk_counter[4] == 1) begin
-            xclock <= 0;
-            spi_ack <= 0;
-        end
-    end
-
-   reg spi_reset;
-   wire spi_addr_buffer_free;
-   reg spi_addr_en;
-   reg [23:0] spi_addr_data;
    wire spi_rd_data_available;
    reg spi_rd_ack;
    reg [15:0] wr_data;
-   wire rd_flag_echo;
-
    reg rd_data_available_old;
+
+   reg [20:0] clk_counter;
+   always @(posedge clk) begin
+       clk_counter <= clk_counter + 1;
+   end
+
 
     spi_writer spi_master_inst(.clk(clk_counter[10]), .reset(rst),
           .SPI_SCK(SPI_SCK), .SPI_SS(SPI_SS), .SPI_MOSI(SPI_MOSI), .SPI_MISO(SPI_MISO),
           .addr_buffer_free(spi_addr_buffer_free), .addr_en(1), .addr_data(24'h100000),
-          .rd_data_available(spi_rd_data_available), .rd_ack(spi_rd_ack), .wr_data(ram_addr)
+          .rd_data_available(spi_rd_data_available), .rd_ack(spi_rd_ack), .wr_data(wr_data)
     );
 
 endmodule
