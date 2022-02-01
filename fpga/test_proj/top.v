a330d7152018d58ccee0d21f01ba07043a516160
kabramovich <wieker@gmail.com>
2022-02-01 23:26:49 +0200

    ICE5 example???

--- fpga/test_proj/top.v
+++ fpga/test_proj/top.v
@@ -1,11 +1,22 @@
 module top(
     output LED1, LED2,
-    input btn1, btn2,
 );
 
+    wire clk;
 
+    SB_HFOSC inthosc (
+      .CLKHFPU(1'b1),
+      .CLKHFEN(1'b1),
+      .CLKHF(clk)
+    );
 
-    assign LED1 = btn1;
-    assign LED2 = btn2;
+    reg [10:0] counter;
+
+    always @(posedge clk) begin
+        counter <= counter + 1;
+    end
+
+    assign LED1 = counter[10];
+    assign LED2 = counter[9];
 
 endmodule
