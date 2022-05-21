a120cf1a3076f556449e863f0815ce68be7ac6cd
kabramovich <wieker@gmail.com>
2022-05-21 15:36:32 +0300

    started PLL TX / undersampling / XTAL phase research / PHASE BORDER research

--- fpga/dsp/dsp_tx_android/osc.v
+++ fpga/dsp/dsp_tx_android/osc.v
@@ -36,15 +36,32 @@ module osc(
 
 // FILTER_RANGE: 1 (3'b001)
 
+// $ icepll -i 48 -o 150
+
+// F_PLLIN:    48.000 MHz (given)
+// F_PLLOUT:  150.000 MHz (requested)
+// F_PLLOUT:  150.000 MHz (achieved)
+
+// FEEDBACK: SIMPLE
+// F_PFD:   24.000 MHz
+// F_VCO:  600.000 MHz
+
+// DIVR:  1 (4'b0001)
+// DIVF: 24 (7'b0011000)
+// DIVQ:  2 (3'b010)
+
+// FILTER_RANGE: 2 (3'b010)
+
+
 
 
    SB_PLL40_CORE #(
           .FEEDBACK_PATH("SIMPLE"),
           .PLLOUT_SELECT("GENCLK"),
-          .DIVR(4'b0010),
-          .DIVF(7'b0110001),
-          .DIVQ(3'b011),
-          .FILTER_RANGE(3'b001),
+          .DIVR(4'b0001),
+          .DIVF(7'b0011000),
+          .DIVQ(3'b010),
+          .FILTER_RANGE(3'b010),
         ) SB_PLL40_CORE_inst (
           .RESETB(1'b1),
           .BYPASS(1'b0),
