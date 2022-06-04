61bb6f1b16fce96d276234bd7f17eeab6f1fbd75
kabramovich <wieker@gmail.com>
2022-06-05 02:46:44 +0300

    started phase DSP: ping time

--- fpga/phase_dsp/phase_ping/correlator.v
+++ fpga/phase_dsp/phase_ping/correlator.v
@@ -11,12 +11,31 @@ module correlator(
 	begin
 	    sig_buf <= sig;
 	    code_buf <= code;
-		if (rst) begin
+	    ram_addr <= ram_addr + 1;
+		if (ram_addr == 0) begin
 		    value <= match_counter[7:0];
 		    match_counter <= 0;
 		end else begin
 		    match_counter <= match_counter + (code_buf == sig_buf);
 		end
 	end
+
+    assign ram_data_in[0] = (code_buf == sig_buf);
+    reg [7:0] ram_addr;
+    wire [15:0] ram_data_in;
+    wire [15:0] ram_data_out;
+
+    SB_RAM40_4K SB_RAM40_4K_inst (
+        .RDATA(ram_data_out),
+        .RADDR(ram_addr),
+        .RCLK(clk),
+        .RCLKE(1),
+        .RE(1),
+        .WADDR(ram_addr),
+        .WCLK(clk),
+        .WCLKE(1),
+        .WDATA(ram_data_in),
+        .WE(1)
+    );
 
 endmodule
