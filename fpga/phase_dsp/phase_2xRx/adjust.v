8b15b824a384680ea7d9873301277e834befc992
kabramovich <wieker@gmail.com>
2022-06-12 23:21:39 +0300

    started phase DSP: temporary switched two channels => single channel

--- fpga/phase_dsp/phase_2xRx/adjust.v
+++ fpga/phase_dsp/phase_2xRx/adjust.v
@@ -8,7 +8,7 @@ module adjust(
     begin
         period <= period + 1;
     end
-    wire [11:0] state = period + 11'h003;
+    wire [11:0] state = period + 11'h005;
 
 	SB_IO #(
 		.PIN_TYPE(6'b101001)
