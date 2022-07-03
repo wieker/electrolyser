c14257e8357133b33be08b816e9c1161760033d8
kabramovich <wieker@gmail.com>
2022-07-03 14:25:22 +0300

    DC POWER filter for AFE: test

--- fpga/phase_dsp/phase_2xRx/adjust1.v
+++ fpga/phase_dsp/phase_2xRx/adjust1.v
@@ -8,7 +8,7 @@ module adjust1(
     begin
         period <= period + 1;
     end
-    wire [11:0] state = period + 11'h002;
+    wire [11:0] state = period + 11'h003;
 
 	SB_IO #(
 		.PIN_TYPE(6'b101001)
