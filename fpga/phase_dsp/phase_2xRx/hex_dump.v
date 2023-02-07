e4823a9fe469bffd5f23b47d1943237067b06d06
kabramovich <wieker@gmail.com>
2023-02-07 15:02:22 +0200

    try 9.6 MHZ 2xTX I/Q dump

--- fpga/phase_dsp/phase_2xRx/hex_dump.v
+++ fpga/phase_dsp/phase_2xRx/hex_dump.v
@@ -17,39 +17,27 @@ module hex_dump(
     end
 
     dispatcher dispatcher(.clk(clk), .rst_in(rst), .sig(sig),
-        .value(i_value));
+        .i_value(i_value), .q_value(q_value));
 
-    reg [16:0] ram_addr;
+    reg [7:0] ram_addr;
     wire [15:0] ram_data_in = {q_value[7:0], i_value[7:0]};
     wire [15:0] ram_data_out;
-    wire [15:0] ram_data_out_tmp[4];
-    wire ram_wren = !ram_addr[16] && stb;
+    wire ram_wren = !ram_addr[7] && stb;
 
-    genvar i;
-    for (i = 0; i < 4; i ++) begin
-        SB_SPRAM256KA spram
-        (
-            .ADDRESS(ram_addr[13:0]),
-            .DATAIN(ram_data_in),
-            .MASKWREN({ram_wren, ram_wren, ram_wren, ram_wren}),
-            .WREN(ram_wren),
-            .CHIPSELECT(i == ram_addr[15:14]),
-            .CLOCK(clk),
-            .STANDBY(1'b0),
-            .SLEEP(1'b0),
-            .POWEROFF(1'b1),
-            .DATAOUT(ram_data_out_tmp[i])
-        );
-    end
+    SB_SPRAM256KA spram
+    (
+        .ADDRESS(ram_addr[6:0]),
+        .DATAIN(ram_data_in),
+        .MASKWREN({ram_wren, ram_wren, ram_wren, ram_wren}),
+        .WREN(ram_wren),
+        .CHIPSELECT(1),
+        .CLOCK(clk),
+        .STANDBY(1'b0),
+        .SLEEP(1'b0),
+        .POWEROFF(1'b1),
+        .DATAOUT(ram_data_out)
+    );
 
-
-	always @(*)
-		casez(ram_addr[15:14])
-			0: ram_data_out = ram_data_out_tmp[0];
-			1: ram_data_out = ram_data_out_tmp[1];
-			2: ram_data_out = ram_data_out_tmp[2];
-			3: ram_data_out = ram_data_out_tmp[3];
-		endcase
 
     reg tx_start;
     reg [7:0] touart;
@@ -60,7 +48,7 @@ module hex_dump(
     begin
         if (ram_wren) begin
             ram_addr <= ram_addr + 1;
-        end else if (ram_addr[16] && !tx_busy && !bugfix001) begin
+        end else if (ram_addr[7] && !tx_busy && !bugfix001) begin
             bugfix001 <= 1;
             tx_start <= 1;
             touart <= part ? ram_data_out[15:8] : ram_data_out[7:0];
