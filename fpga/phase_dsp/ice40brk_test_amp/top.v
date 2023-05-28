c52f0c15a06b6acfdc0f9159d9120ea1ad736a58
kabramovich <wieker@gmail.com>
2023-05-28 22:19:02 +0300

    started ICE40-brk AMP test

--- fpga/phase_dsp/ice40brk_test_amp/top.v
+++ fpga/phase_dsp/ice40brk_test_amp/top.v
@@ -11,5 +11,21 @@ module top(
     wire comp_in;
 	digitizer digitizer(.clk(clk), .rst(rst), .lvds_in(lvds_in), .sig(sig_in), .comp_in(comp_in));
 
+	wire[7:0] ones;
+	wire[7:0] zeros;
+	correlator one(.clk(clk), .rst(rst), .code(1), .sig(sig_in), .value(ones));
+	correlator zero(.clk(clk), .rst(rst), .code(0), .sig(sig_in), .value(zeros));
 
+	assign LED1 = ones[7];
+	assign LED2 = ones[6];
+	assign LED3 = ones[5];
+	assign LED4 = ones[4];
+
+    reg [10:0] tx_counter;
+    always@(posedge xtal_in)
+    begin
+        tx_counter <= tx_counter + 1;
+    end
+
+    assign tx_out = tx_counter[3];
 endmodule
