2294f75cc4cd750c86a9f7d750859118b9171dcf
kabramovich <wieker@gmail.com>
2023-07-04 00:08:18 +0300

    started ICE40-brk AMP test / 128 / 256 parts counter

--- fpga/phase_dsp/ice40brk_test_amp/top.v
+++ fpga/phase_dsp/ice40brk_test_amp/top.v
@@ -11,18 +11,17 @@ module top(
     wire comp_in;
 	digitizer digitizer(.clk(clk), .rst(rst), .lvds_in(lvds_in), .sig(sig_in), .comp_in(comp_in));
 
-	reg [9:0] counter;
-	reg ou1, ou2;
-	reg [9:0] on;
+	reg [8:0] counter;
+	reg ou1;
+	reg [8:0] on;
 
     reg [10:0] tx_counter;
     always@(posedge xtal_in)
     begin
         tx_counter <= tx_counter + 1;
-        if (counter[9] == 1 && counter[8] == 1) begin
+        if (counter[8] == 1) begin
             counter <= 0;
-            ou1 <= on[7] | on[8] | on[9];
-            ou2 <= on[8] | on[9];
+            ou1 <= on[8] | on[7];
             on <= sig_in;
         end else begin
             counter <= counter + 1;
@@ -30,6 +29,5 @@ module top(
         end
     end
 
-    assign LED2 = ou1;
-    assign LED3 = ou2;
+    assign tx_out = ou1;
 endmodule
