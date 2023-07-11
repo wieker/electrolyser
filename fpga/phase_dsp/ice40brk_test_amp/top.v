56d92910d0601f30fd451e6bb98dde2b2c3c4b58
kabramovich <wieker@gmail.com>
2023-07-11 18:22:24 +0300

    started ICE40-brk

--- fpga/phase_dsp/ice40brk_test_amp/top.v
+++ fpga/phase_dsp/ice40brk_test_amp/top.v
@@ -3,7 +3,7 @@ module top(
     input lvds_in, xtal_in,
 );
 
-    wire clk = xtal_in;
+    wire clk;
     wire rst;
     osc osc(.clk(clk), .rst(rst));
 
@@ -16,7 +16,7 @@ module top(
 	reg [8:0] on;
 
     reg [10:0] tx_counter;
-    always@(posedge xtal_in)
+    always@(posedge clk)
     begin
         tx_counter <= tx_counter + 1;
         if (counter[8] == 1) begin
@@ -28,6 +28,36 @@ module top(
             on <= on + sig_in;
         end
     end
+
+//    $ icepll -o 20 -i 12
+
+//    F_PLLIN:    12.000 MHz (given)
+//    F_PLLOUT:   20.000 MHz (requested)
+//    F_PLLOUT:   19.875 MHz (achieved)
+
+//    FEEDBACK: SIMPLE
+//    F_PFD:   12.000 MHz
+//    F_VCO:  636.000 MHz
+
+//    DIVR:  0 (4'b0000)
+//    DIVF: 52 (7'b0110100)
+//    DIVQ:  5 (3'b101)
+
+//    FILTER_RANGE: 1 (3'b001)
+
+   SB_PLL40_CORE #(
+          .FEEDBACK_PATH("SIMPLE"),
+          .PLLOUT_SELECT("GENCLK"),
+          .DIVR(4'b0000),
+          .DIVF(7'b0110100),
+          .DIVQ(3'b101),
+          .FILTER_RANGE(3'b001),
+        ) SB_PLL40_CORE_inst (
+          .RESETB(1'b1),
+          .BYPASS(1'b0),
+          .PLLOUTCORE(clk),
+          .REFERENCECLK(xtal_in)
+    );
 
     assign tx_out = ou1;
 endmodule
