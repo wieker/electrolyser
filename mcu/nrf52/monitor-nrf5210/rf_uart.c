fde5a0cdee45229af015ff9f5fced1dbd86d7b45
Your Name <you@example.com>
2023-09-07 22:10:07 +0400

    debug 1D

--- mcu/nrf52/monitor-nrf5210/rf_uart.c
+++ mcu/nrf52/monitor-nrf5210/rf_uart.c
@@ -1,10 +1,7 @@
 #include <nrf_delay.h>
 #include <nrf_esb.h>
 
-#include <nrf_local_adc.h>
-
 #include "local_nrf_esb.h"
-
 #include <legacy/nrf_drv_uart.h>
 #include "ble_config/sdk_config.h"
 
@@ -39,50 +36,30 @@ void cpyN(int n, uint8_t* string, char* dest) {
 }
 
 void advertise() {
-  // char data[5];
-  // nrf_saadc_value_t vV = m_buffer_pool[0][0];
-  // nrf_saadc_value_t vI = m_buffer_pool[0][1];
-  // data[0] = (vV / 16) < 10 ? (vV / 16) + '0' : (vV / 16) + '7';
-  // data[1] = (vV % 16) < 10 ? (vV % 16) + '0' : (vV % 16) + '7';
-  // data[2] = (vI / 16) < 10 ? (vI / 16) + '0' : (vI / 16) + '7';
-  // data[3] = (vI % 16) < 10 ? (vI % 16) + '0' : (vI % 16) + '7';
-  // data[4] = '\n';
   radio_packet_send((uint8_t *) dt, strlen(dt));
   //nrfx_uart_tx(&m_uart.uart, (uint8_t  *) "txt\n", 4);
 }
 
 void radio_packet_recv(uint8_t *packet, uint32_t packet_length) {
   nrfx_uart_tx(&m_uart.uart, packet, packet_length);
-  //cpy(packet, dt);
 }
 
 int main() {
-  NRF_P0->DIRSET = 1 << 17 | 1 << 18 | 1 << 19 | 1 << 20 | 1 << 30 | 1 << 31;
+  NRF_P0->DIRSET = 1 << 17 | 1 << 18 | 1 << 19 | 1 << 20;
   uart_init();
 
   radio_init(1);
 
-  NRF_P0->OUTSET = 1 << 30;
-
-
-  //saadc_init();
-  //saadc_sampling_event_init();
-  //saadc_sampling_event_enable();
-
   while (1) {
     NRF_P0->OUTSET = 1 << 17 | 1 << 20;
     NRF_P0->OUTCLR = 1 << 18 | 1 << 19;
-    NRF_P0->OUTSET = 1 << 31;
     nrf_delay_ms(1000);
 
     NRF_P0->OUTSET = 1 << 18 | 1 << 19;
     NRF_P0->OUTCLR = 1 << 17 | 1 << 20;
-    NRF_P0->OUTCLR = 1 << 31;
     nrf_delay_ms(1000);
 
     //nrfx_uart_tx(&m_uart.uart, (uint8_t  *) "ok \r\n", 5);
-
-    advertise();
   }
 }
 
