11d7d1233ce2f570c9b73cc686f732190a4c46f8
Your Name <you@example.com>
2023-09-23 00:34:19 +0400

    debug 1D

--- mcu/lpc/ctl/ctl/sensors.c
+++ mcu/lpc/ctl/ctl/sensors.c
@@ -190,7 +190,7 @@ static void GYRO_Common(void)
     for (axis = 0; axis < 3; axis++)
         gyroADC[axis] -= gyroZero[axis];
     accADC[2] -= 6;
-    accADC[1] += 5;
+    accADC[1] += 7;
     accADC[0] -= 9;
 }
 
