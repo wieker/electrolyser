7ba0540be368c4bf82dbd46c1d7fedc80bb264e3
Your Name <you@example.com>
2023-10-11 16:17:07 +0400

    debug 1D

--- mcu/lpc/ctl/main.c
+++ mcu/lpc/ctl/main.c
@@ -113,7 +113,7 @@ static void pidMultiWii(void)
     int32_t PTerm, ITerm, DTerm;
     static int32_t lastError[3] = { 0, 0, 0 };
     int32_t AngleRateTmp, RateError;
-    int32_t cfgP8 = 300;
+    int32_t cfgP8 = 400;
     int32_t cfgI8 = 300;
     int32_t cfgD8 = 200;
     int32_t cfgP8PIDLEVEL = 15;
