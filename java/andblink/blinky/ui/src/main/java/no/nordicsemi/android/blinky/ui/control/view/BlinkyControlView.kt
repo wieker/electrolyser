2c5ea86f9218fc1761b59565c0a1f46e9adb89c9
Your Name <you@example.com>
2023-11-19 01:36:09 +0400

    timer => adc

--- java/andblink/blinky/ui/src/main/java/no/nordicsemi/android/blinky/ui/control/view/BlinkyControlView.kt
+++ java/andblink/blinky/ui/src/main/java/no/nordicsemi/android/blinky/ui/control/view/BlinkyControlView.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import com.MobileAnarchy.Android.Widgets.Joystick.JoystickView
 import com.longdo.mjpegviewer.MjpegView
 import com.longdo.mjpegviewer.MjpegViewUDP
 
@@ -109,6 +110,11 @@ internal fun BlinkyControlView(
                 enabled = sliderProcess,
                 steps = 33,
             )
+
+            AndroidView(factory = {context ->
+                JoystickView(context)
+            },
+                modifier = Modifier.alpha(0.5f))
         }
     }
 }
