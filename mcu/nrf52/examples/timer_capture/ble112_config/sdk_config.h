20f51a96554003048ce4e9060fc0a5cf231a3265
Your Name <you@example.com>
2024-04-21 22:43:43 +0400

    fix UART logging build

--- mcu/nrf52/examples/timer_capture/ble112_config/sdk_config.h
+++ mcu/nrf52/examples/timer_capture/ble112_config/sdk_config.h
@@ -4300,7 +4300,7 @@
 #endif
 // <o> NRFX_UARTE0_ENABLED - Enable UARTE0 instance
 #ifndef NRFX_UARTE0_ENABLED
-#define NRFX_UARTE0_ENABLED 1
+#define NRFX_UARTE0_ENABLED 0
 #endif
 
 // <o> NRFX_UARTE_DEFAULT_CONFIG_HWFC  - Hardware Flow Control
@@ -4421,7 +4421,7 @@
 #endif
 // <o> NRFX_UART0_ENABLED - Enable UART0 instance
 #ifndef NRFX_UART0_ENABLED
-#define NRFX_UART0_ENABLED 1
+#define NRFX_UART0_ENABLED 0
 #endif
 
 // <o> NRFX_UART_DEFAULT_CONFIG_HWFC  - Hardware Flow Control
