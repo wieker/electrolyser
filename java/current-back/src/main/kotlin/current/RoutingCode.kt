1ba19c79c6ba51f8207d0ebb801afb4e7408468e
Your Name <you@example.com>
2024-09-01 00:14:14 +0400

    current monitor back-end

--- java/current-back/src/main/kotlin/current/RoutingCode.kt
+++ java/current-back/src/main/kotlin/current/RoutingCode.kt
@@ -2,6 +2,7 @@ package org.example.current
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.*
@@ -39,6 +40,19 @@ fun Application.configureRouting() {
             get {
                 call.respond(
                     ThymeleafContent("measures", mapOf("measures" to listOf(Measure("t,", LocalDate.now(), 0.1, 0.2))))
+                )
+            }
+        }
+
+        route("/measure/add") {
+            post {
+                val someValue = call.receiveParameters()["priority"]
+                call.respond(
+                    ThymeleafContent("measures",
+                        mapOf(
+                                "measures" to listOf(Measure("t,", LocalDate.now(), 0.1, 0.2)),
+                                "someValue" to someValue!!
+                            ))
                 )
             }
         }
