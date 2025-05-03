b03385e3dd7eabc08820910169938970b24621fc
Your Name <you@example.com>
2025-05-03 22:22:58 +0400

    lambda experiment

--- java/simple_host/src/main/java/org/allesoft/correlator/experiment/Lambda.java
+++ java/simple_host/src/main/java/org/allesoft/correlator/experiment/Lambda.java
@@ -1,4 +1,38 @@
 package org.allesoft.correlator.experiment;
 
 public class Lambda {
+    class Element {
+        String name;
+    }
+
+    class InputSeq {
+        String str;
+        int pos;
+    }
+
+    public boolean parseLiteral(InputSeq input) {
+        return false;
+    }
+
+    public boolean parseLambda(InputSeq input) {
+        return false;
+    }
+
+    public boolean parseApplication(InputSeq input) {
+        return false;
+    }
+
+    public void parse(InputSeq input) {
+        if (!parseLiteral(input)) {
+            if (!parseLambda(input)) {
+                if (!parseApplication(input)) {
+                    throw new RuntimeException("syntax error");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String program = "";
+    }
 }
