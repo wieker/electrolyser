855eb90aa61d88bdd462135fb7e71016699a0ed9
Your Name <you@example.com>
2025-11-12 22:30:58 +0400

    TIFF => demosaic => BMP

--- java/simple_host/src/main/java/org/allesoft/correlator/experiment/Tiff2Bmp.java
+++ java/simple_host/src/main/java/org/allesoft/correlator/experiment/Tiff2Bmp.java
@@ -6,6 +6,7 @@ import mil.nga.tiff.TIFFImage;
 import mil.nga.tiff.TiffReader;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
@@ -37,5 +38,40 @@ public class Tiff2Bmp {
 
         System.out.println(image.getHeight());
         System.out.println(image.getWidth());
+
+
+        System.out.println("Supported Read Formats:");
+        String[] readerFormats = ImageIO.getReaderFormatNames();
+        for (String format : readerFormats) {
+            System.out.println("- " + format);
+        }
+
+        System.out.println("\nSupported Write Formats:");
+        String[] writerFormats = ImageIO.getWriterFormatNames();
+        for (String format : writerFormats) {
+            System.out.println("- " + format);
+        }
+
+
+        BufferedImage resizedImage = new BufferedImage(image.getWidth() / 2, image.getHeight() / 2, BufferedImage.TYPE_INT_RGB);
+        System.out.println(image.getType());
+        for (int i = 0; i < resizedImage.getWidth(); i ++) {
+            for (int j = 0; j < resizedImage.getHeight(); j ++) {
+                int redVal = Short.toUnsignedInt(((short[]) image.getRaster().getDataElements(i * 2, j * 2, (Object) null))[0]) >> 8;
+                int greenVal = Short.toUnsignedInt(((short[]) image.getRaster().getDataElements(i * 2 + 1, j * 2, (Object) null))[0]) >> 8;
+                int blueVal = Short.toUnsignedInt(((short[]) image.getRaster().getDataElements(i * 2 + 1, j * 2 + 1, (Object) null))[0]) >> 8;
+                resizedImage.setRGB(i, j, new Color(
+                        redVal,
+                        greenVal,
+                        blueVal)
+                        .getRGB());
+            }
+        }
+        boolean success = ImageIO.write(resizedImage, "BMP", new File("/home/wieker/output.bmp"));
+        if (success) {
+            System.out.println("BMP image saved successfully.");
+        } else {
+            System.out.println("Failed to save BMP image. Check image type compatibility.");
+        }
     }
 }
