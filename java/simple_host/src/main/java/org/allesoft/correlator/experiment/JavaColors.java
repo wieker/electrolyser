1d319e11a36472a4e49704109445d9a76bb7b862
Your Name <you@example.com>
2025-11-16 21:47:55 +0400

    TIFF => demosaic => XYZ => gamma => BMP

---
+++ java/simple_host/src/main/java/org/allesoft/correlator/experiment/JavaColors.java
@@ -0,0 +1,17 @@
+package org.allesoft.correlator.experiment;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class JavaColors {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Power dumper");
+
+        frame.setMinimumSize(new Dimension(1920, 1080));
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
