a1ab204e236e3e498c5390eeacfffa6157ce081d
Your Name <you@example.com>
2025-12-24 19:21:38 +0400

    fpga fast SPI ifc

--- host_c/iceprog/java_bridge.c
+++ host_c/iceprog/java_bridge.c
@@ -3,7 +3,11 @@
 #include "org_allesoft_correlator_experiment_JavaBridge.h"
 #include <jni.h> // Include the JNI header
 
+int main(int argc, char **argv);
+
 JNIEXPORT void JNICALL Java_org_allesoft_correlator_experiment_JavaBridge_print(JNIEnv *env, jobject obj) {
     printf("Printing from native\\n");
+    char *args[3] = {"./iceprog", "-S", "/home/wieker/Projects/electrolyser/fpga/test_spi/top.bin"};
+    main(3, args);
 }
 
