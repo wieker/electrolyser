0aa93330d0f44efab6298fbf777fbbf6a72864a9
Your Name <you@example.com>
2025-12-24 18:53:00 +0400

    fpga fast SPI ifc

---
+++ java/simple_host/src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    static {
+        System.loadLibrary("HelloWorld");
+    }
+
+    private native void print();
+
+    //application main entry point
+    public static void main(String[] args) {
+
+        //invoke non-static print method
+        new HelloWorld().print();
+    }
+}
