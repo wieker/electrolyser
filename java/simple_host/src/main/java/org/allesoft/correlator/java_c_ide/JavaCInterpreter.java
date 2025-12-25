670aec4baacfde5eec358fdbc7327c13107c72ac
Your Name <you@example.com>
2025-12-25 13:30:08 +0400

    Java C interpreter

---
+++ java/simple_host/src/main/java/org/allesoft/correlator/java_c_ide/JavaCInterpreter.java
@@ -0,0 +1,15 @@
+package org.allesoft.correlator.java_c_ide;
+
+public class JavaCInterpreter {
+    /*
+        Java C IDE plugin for IntelliJ community:
+        - GNU Emacs GUI + Emacs Lisp + lsp-mode/clangd
+        - VS Code GUI + JavaScript + C++ LSP/clangd
+        - IntelliJ open source GUI + Java + cmake/bear compile_commands.json => Eclipse CDT/LSP parser/interpreter for C => IntelliJ PSI
+
+        so
+        1. Run C Interpreter from https://github.com/wieker/java-c-interpreter
+        2. Read compile_commands.json
+        3. Port to IDEA GUI => PSI
+     */
+}
