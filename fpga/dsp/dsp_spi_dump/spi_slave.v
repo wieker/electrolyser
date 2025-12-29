7d9c60184812aad8940b47449833c62a47490914
Your Name <you@example.com>
2025-12-29 22:23:05 +0400

    Java Bridge: SPI => dual port ice40 SRAM => SPI. prepare DSP => ice40 SRAM => SPI bridge

---
+++ fpga/dsp/dsp_spi_dump/spi_slave.v
@@ -0,0 +1,66 @@
+// receive: byte2 | byte1 | byte0 | opcode/status
+//read all the data, but can write only the two bytes as opcode contains metadata
+
+module spi_slave(input wire clk, input wire reset,
+      input wire SPI_SCK, input wire SPI_SS, input wire SPI_MOSI, output wire SPI_MISO,
+      output reg wr_buffer_free, input wire wr_en, input wire [0:15] wr_data,
+      output reg rd_data_available, input wire rd_ack, output reg [0:15] rd_data,
+      output wire [2:0] cnt
+   );
+
+   reg [0:4] counter_read; //max 32
+   reg [0:15] wr_reg;
+   reg [0:15] rd_reg;
+
+  reg [1:0] spi_clk_reg;
+  reg [1:0] spi_ss_reg;
+  reg [1:0] spi_mosi_reg;
+  wire spi_ss_falling_edge;
+  wire spi_ss_rising_edge;
+  wire spi_clk_rising_edge;
+  wire spi_clk_falling_edge;
+
+  assign cnt = (counter[4] == 0) ? rd_reg[0] : wr_reg[0];
+
+   assign spi_clk_rising_edge = (spi_clk_reg[1:0] == 2'b01);
+   assign spi_clk_falling_edge = (spi_clk_reg[1:0] == 2'b10);
+   assign spi_ss_rising_edge = (spi_ss_reg[1:0] == 2'b01);
+   assign spi_ss_falling_edge = (spi_ss_reg[1:0] == 2'b10);
+
+    reg [0:4] counter;
+   assign SPI_MISO = wr_reg[15];
+
+
+   always @(posedge clk)
+   begin
+      if(reset == 1) begin
+         rd_data <= 0;
+         rd_data_available <= 0;
+      end else begin
+         spi_clk_reg <= {spi_clk_reg[0], SPI_SCK};
+         spi_mosi_reg <= {spi_mosi_reg[0], SPI_MOSI};
+         spi_ss_reg <= {spi_ss_reg[0], SPI_SS};
+
+         if(spi_ss_rising_edge == 1'b1) begin
+            counter <= 0;
+         end
+
+         if(spi_clk_rising_edge == 1'b1) begin
+            rd_reg <= {spi_mosi_reg[1], rd_reg[0:14]};
+            if (counter == 15) begin
+                counter <= 0;
+                rd_data_available <= 1;
+                wr_reg <= wr_data;
+                rd_data <= {spi_mosi_reg[1], rd_reg[0:14]};
+            end else begin
+                counter <= counter + 1;
+                wr_reg <= {0, wr_reg[0:14]};
+            end
+         end
+
+         if (rd_data_available == 1) begin
+            rd_data_available <= 0;
+         end
+      end
+   end
+endmodule
---
+++ fpga/dsp/dsp_spi_dump/top.v
@@ -0,0 +1,86 @@
+`include "spi_slave.v"
+
+//opcodes:
+//0x00 nop
+//0x01 init
+//0x02 write 16bits inverted
+//0x03 read 16bits inverted
+//0x04 write leds (16bits LSB)
+//0x05 read leds (16bits LSB)
+//0x06 write vector, the computer will send 4 * 24bit values
+//0x07 read vector, the fpga will send 4 * 24bit values
+
+module top(input [3:0] SW, input clk, output LED_R, output LED_G, output LED_B, input SPI_SCK, input SPI_SS, input SPI_MOSI, output SPI_MISO, input [3:0] SW);
+
+   reg spi_reset;
+   wire spi_rd_data_available;
+   reg spi_rd_ack;
+   wire [0:15] spi_rd_data;
+
+   //assign SPI_MISO = SPI_MOSI;
+   wire tmp;
+
+   spi_slave spi_slave_inst(.clk(clk), .reset(spi_reset),
+      .SPI_SCK(SPI_SCK), .SPI_SS(SPI_SS), .SPI_MOSI(SPI_MOSI), .SPI_MISO(SPI_MISO),
+      .wr_buffer_free(spi_wr_buffer_free), .wr_en(spi_wr_en), .wr_data(ram_data_out),
+      .rd_data_available(spi_rd_data_available), .rd_ack(spi_rd_ack), .rd_data(spi_rd_data),
+      .cnt(cnt)
+   );
+
+   wire [2:0] cnt;
+
+   reg [2:0] led;
+
+   assign LED_R = cnt[0];
+   assign LED_G = cnt[1];
+   assign LED_B = cnt[2];
+
+   integer i;
+
+   initial begin
+
+      for(i = 0; i < 4; i=i+1) begin
+         vector[i] = 0;
+      end
+
+      spi_reset = 0;
+      spi_wr_en = 0;
+      spi_wr_data = 0;
+      spi_rd_ack = 0;
+
+      vec_ptr = 0;
+      sending_vector = 0;
+
+      led = 0;
+      spi_recv_data_reg = 0;
+      handle_data = 0;
+   end
+
+
+    reg [2:0] ram_wr_addr;
+    reg [2:0] ram_rd_addr;
+    wire [0:15] ram_data_out;
+
+    SB_RAM40_4K SB_RAM40_4K_inst (
+        .RDATA(ram_data_out),
+        .RADDR(ram_rd_addr),
+        .RCLK(clk),
+        .RCLKE(1),
+        .RE(1),
+        .WADDR(ram_wr_addr),
+        .WCLK(clk),
+        .WCLKE(1),
+        .WDATA(spi_rd_data),
+        .WE(spi_rd_data_available)
+    );
+
+   always @(posedge clk)
+   begin
+
+      if(spi_rd_data_available == 1) begin // rising edge
+         ram_rd_addr <= ram_rd_addr + 1;
+         ram_wr_addr <= ram_wr_addr + 1;
+      end
+   end
+
+endmodule
