85f73b781039727061e00774d1ee5f69fa550208
Your Name <you@example.com>
2026-03-29 18:58:46 +0400

    Galois solver for quintics in numeric way

---
+++ java/simple_host/src/main/java/org/allesoft/correlator/java_c_ide/GaloisSolver.java
@@ -0,0 +1,16 @@
+package org.allesoft.correlator.java_c_ide;
+
+public class GaloisSolver {
+    public static void main(String[] args) {
+
+    }
+
+    class Ratio {
+        byte[] a;
+        byte[] b;
+    }
+
+    class Irratio {
+        byte[] a;
+    }
+}
